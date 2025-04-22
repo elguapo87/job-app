7c936aba6ff3806c03092508ad717ca8be451891
elguapo87 <pglogovac@gmail.com>
2025-04-22 23:17:06 +0200

    Created AppContext & setup in main.tsx

---
+++ client/src/context/AppContext.tsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+
+interface AppContextType {};
+
+export const AppContext = createContext<AppContextType | undefined>(undefined);
+
+const AppContextProvider = ({ children }: { children: React.ReactNode }) => {
+
+    const value = {};
+
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+};
+
+export default AppContextProvider;
