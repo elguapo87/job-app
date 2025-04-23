98cd7b7ba627f880fb72df2f4738ab106d58f6a8
elguapo87 <pglogovac@gmail.com>
2025-04-23 14:05:01 +0200

    Added jobs state & defined type in context API

--- client/src/context/AppContext.tsx
+++ client/src/context/AppContext.tsx
@@ -1,8 +1,26 @@
 import { createContext, useState } from "react";
+import { jobsData } from "../assets/assets";
 
 interface SearchFilter {
     title: string;
     location: string
+}
+
+interface JobsTypes {           
+    _id: string;
+    title: string;
+    location: string;
+    level: string;
+    companyId: {
+        _id: string;
+        name: string;
+        email: string;
+        image: string
+    };
+    description: string;
+    salary: number;
+    date: number;
+    category: string;
 }
 
 interface AppContextType {
@@ -10,6 +28,8 @@ interface AppContextType {
     setSearchFilter: React.Dispatch<React.SetStateAction<SearchFilter>>;
     isSearched: boolean;
     setIsSearched: React.Dispatch<React.SetStateAction<boolean>>;
+    jobs: JobsTypes[];                                                  
+    setJobs: React.Dispatch<React.SetStateAction<JobsTypes[]>>         
 }
 
 export const AppContext = createContext<AppContextType | undefined>(undefined);
@@ -22,10 +42,17 @@ const AppContextProvider = ({ children } : { children: React.ReactNode }) => {
     });
 
     const [isSearched, setIsSearched] = useState(false);
+    const [jobs, setJobs] = useState<JobsTypes[]>([]);
+
+    // Function to fetch job data                          
+    const fetchJobs = async () => {
+        setJobs(jobsData);
+    };
 
     const value = {
         searchFilter, setSearchFilter,
-        isSearched, setIsSearched
+        isSearched, setIsSearched,
+        jobs, setJobs                               
     }
 
     return (
