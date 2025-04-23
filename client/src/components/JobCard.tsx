7e663cdaf198c19033d229a1ab3db45a71b95e80
elguapo87 <pglogovac@gmail.com>
2025-04-23 19:11:06 +0200

    Added & created Loading component

--- client/src/components/JobCard.tsx
+++ client/src/components/JobCard.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets"
 
 type JobListingTypes = {
@@ -18,11 +19,15 @@ type JobListingTypes = {
 }
 
 const JobCard = ({ job } : { job: JobListingTypes}) => {
+
+  const navigate = useNavigate();                             
+
   return (
     <div className="border p-6 shadow rounded">
       <div className="flex justify-between items-center">
         <img className="h-8" src={assets.company_icon} alt="" />
       </div>
+
       <h4 className="font-medium text-xl mt-2">{job.title}</h4>
 
       <div className="flex items-center gap-3 mt-2 text-xs">
@@ -33,7 +38,7 @@ const JobCard = ({ job } : { job: JobListingTypes}) => {
       <p className="text-gray-500 text-sm mt-4" dangerouslySetInnerHTML={{__html: job.description.slice(0, 150)}}></p>
 
       <div className="mt-4 flex gap-4 text-sm">
-        <button className="bg-blue-600 text-white px-4 py-2 rounded">Apply now</button>
+        <button onClick={() => { navigate(`/apply-job/${job._id}`); scrollTo(0,0) }} className="bg-blue-600 text-white px-4 py-2 rounded">Apply now</button>   ( dodati onClick )
         <button className="text-gray-500 border border-gray-500 px-4 py-2 rounded">Learn more</button>
       </div>
     </div>
---
+++ client/src/components/Loading.tsx
@@ -0,0 +1,13 @@
+const Loading = ({ position } : { position: "normal" | "center" }) => {
+    return (
+      <div className={`w-screen h-screen flex items-center justify-center ${position === "center" && "-translate-x-[8%] -translate-y-[6%]"}`}>
+          <div className="flex flex-col items-center gap-2">
+              <div className="w-20 sm:w-[150px] h-20 sm:h-[150px] border-[8px] sm:border-[15px] border-gray-300 border-t-[8px] sm:border-t-[15px] border-t-blue-400 rounded-full animate-spin"></div>
+              <p className="text-3xl sm:text-5xl font-semibold text-blue-500">Loading...</p>
+          </div>
+      </div>
+    )
+  }
+  
+  export default Loading
+  
