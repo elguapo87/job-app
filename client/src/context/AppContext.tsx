c25224836218cba92944c8cb6ef62b74dffbf577
elguapo87 <pglogovac@gmail.com>
2025-04-23 13:57:30 +0200

    Creted states for search filter in context API & include them in Hero compponent

--- client/src/context/AppContext.tsx
+++ client/src/context/AppContext.tsx
@@ -1,12 +1,32 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-interface AppContextType {};
+interface SearchFilter {
+    title: string;
+    location: string
+}
+
+interface AppContextType {
+    searchFilter: SearchFilter;
+    setSearchFilter: React.Dispatch<React.SetStateAction<SearchFilter>>;
+    isSearched: boolean;
+    setIsSearched: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
 export const AppContext = createContext<AppContextType | undefined>(undefined);
 
-const AppContextProvider = ({ children }: { children: React.ReactNode }) => {
+const AppContextProvider = ({ children } : { children: React.ReactNode }) => {
 
-    const value = {};
+    const [searchFilter, setSearchFilter] = useState({
+        title: "",
+        location: ""
+    });
+
+    const [isSearched, setIsSearched] = useState(false);
+
+    const value = {
+        searchFilter, setSearchFilter,
+        isSearched, setIsSearched
+    }
 
     return (
         <AppContext.Provider value={value}>
