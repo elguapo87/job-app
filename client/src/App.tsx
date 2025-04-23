b994a6a4f7b3bbb85bd2531c4c5e78c6f6e3d5fb
elguapo87 <pglogovac@gmail.com>
2025-04-23 19:57:06 +0200

    Created RecruiterLogin component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,20 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import ApplyJob from "./pages/ApplyJob"
 import Applications from "./pages/Applications"
+import RecruiterLogin from "./components/RecruiterLogin"
+import { useContext } from "react"
+import { AppContext } from "./context/AppContext"
 
 const App = () => {
+
+  const context = useContext(AppContext);                                     
+  if (!context) throw new Error("App must be inside AppContextProvider");     
+
+  const { showRecruiterLogin } = context;                                   
+
   return (
     <div>
+      {showRecruiterLogin && <RecruiterLogin />}                             
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/apply-job/:id" element={<ApplyJob />} />
