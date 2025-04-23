0c4640697e21d5f943d7beab5611add18f75dc55
elguapo87 <pglogovac@gmail.com>
2025-04-23 13:48:23 +0200

    Setup clerk

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,15 +1,32 @@
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react"
 import { assets } from "../assets/assets"
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+  const { openSignIn } = useClerk();                 
+  const { user } = useUser();                           
+
   return (
     <div className="shadow py-4">
         <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center">
         <img className="cursor-pointer w-[130px] sm:w-[180px]" src={assets.company_logo} alt="" />
 
-          <div className="flex gap-4 max-sm:text-xs">
-            <button className="text-gray-600">Recruiter Login</button>
-            <button className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full ">Login</button>
-          </div>
+          {
+            user                                 
+             ?
+            <div className="flex items-center gap-3">
+              <Link to="/applications">Applied Jobs</Link>
+              <p>|</p>
+              <p>Hy, {user.firstName && user.lastName ? user.firstName + " " + user.lastName : "John Doe"}</p>
+              <UserButton />
+            </div>
+             :
+            <div className="flex gap-4 max-sm:text-xs">
+              <button className="text-gray-600">Recruiter Login</button>
+              <button onClick={() => openSignIn()} className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full ">Login</button>    
+            </div>
+          }
         </div>
     </div>
   )
