a2ad5a717014fae821290c6e4d71eddc8cb63301
elguapo87 <pglogovac@gmail.com>
2025-04-23 20:08:41 +0200

    Added Dashboard, ManageJobs, ViewApplications, AddJob pages & import that pages in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,21 +5,32 @@ import Applications from "./pages/Applications"
 import RecruiterLogin from "./components/RecruiterLogin"
 import { useContext } from "react"
 import { AppContext } from "./context/AppContext"
+import Dashboard from "./pages/Dashboard"
+import AddJob from "./pages/AddJob"
+import ManageJobs from "./pages/ManageJobs"
+import ViewApplications from "./pages/ViewApplications"
 
 const App = () => {
 
-  const context = useContext(AppContext);                                     
-  if (!context) throw new Error("App must be inside AppContextProvider");     
+  const context = useContext(AppContext);
+  if (!context) throw new Error("App must be inside AppContextProvider");
 
-  const { showRecruiterLogin } = context;                                   
+  const { showRecruiterLogin } = context;
 
   return (
     <div>
-      {showRecruiterLogin && <RecruiterLogin />}                             
+      {showRecruiterLogin && <RecruiterLogin />}  
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/apply-job/:id" element={<ApplyJob />} />
         <Route path="/applications" element={<Applications />} />
+
+        {/* RECRUITER ROUTES */}                               
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="menage-jobs" element={<ManageJobs />} />
+          <Route path="view-applicaitons" element={<ViewApplications />} />
+        </Route>
       </Routes>
     </div>
   )
