427afb8955db4814aac9388052c37c04c07aa73b
elguapo87 <pglogovac@gmail.com>
2025-04-23 21:43:41 +0200

    Created server folder for application backend, installed dependencies & setup basic express server

---
+++ server/src/server.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("API is working");
+});
+
+const startServer = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+
+    } catch (error) {
+        console.error("Server Connection failed");
+        process.exit(1);
+    }
+};
+
+startServer();
