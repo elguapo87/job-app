52cc54cdff8df02db279b1640cd60e3805ef0aca
elguapo87 <pglogovac@gmail.com>
2025-04-24 14:21:56 +0200

    Added logic for close RecruiterLogin in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useNavigate } from "react-router-dom"
 import Home from "./pages/Home"
 import ApplyJob from "./pages/ApplyJob"
 import Applications from "./pages/Applications"
-import RecruiterLogin from "./components/RecruiterLogin"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { AppContext } from "./context/AppContext"
-import Dashboard from "./pages/Dashboard"
+import RecruiterLogin from "./components/RecruiterLogin"
 import AddJob from "./pages/AddJob"
+import Dashboard from "./pages/Dashboard"
 import ManageJobs from "./pages/ManageJobs"
 import ViewApplications from "./pages/ViewApplications"
 import "quill/dist/quill.snow.css"
@@ -15,25 +15,46 @@ import { ToastContainer } from 'react-toastify'
 const App = () => {
 
   const context = useContext(AppContext);
-  if (!context) throw new Error("App must be inside AppContextProvider");
+  if (!context) throw new Error("App must be within AppContextProvider");
+  const { showRecruiterLogin, setShowRecruiterLogin, companyToken } = context;
 
-  const { showRecruiterLogin } = context;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // If the user tries to access recruiter pages without a token, show the login popup
+    if (!companyToken && location.pathname.startsWith("/dashboard")) {
+      setShowRecruiterLogin(true);
+    }
+  }, [companyToken, location.pathname, setShowRecruiterLogin]);
+
+  const handleCloseRecruiterLogin = () => {
+    setShowRecruiterLogin(false);
+    if (!companyToken) {
+      navigate("/");
+    }
+  };
 
   return (
     <div>
-      {showRecruiterLogin && <RecruiterLogin />}  
+      {showRecruiterLogin && <RecruiterLogin onClose={handleCloseRecruiterLogin} />}
       <ToastContainer />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/apply-job/:id" element={<ApplyJob />} />
         <Route path="/applications" element={<Applications />} />
 
-        {/* RECRUITER ROUTES */}                               
-        <Route path="/dashboard" element={<Dashboard />}>
-          <Route path="add-job" element={<AddJob />} />
-          <Route path="menage-jobs" element={<ManageJobs />} />
-          <Route path="view-applicaitons" element={<ViewApplications />} />
-        </Route>
+        {/* Recruiter Routes */}
+        {
+          companyToken
+             ?
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="add-job" element={<AddJob />} />
+            <Route path="manage-jobs" element={<ManageJobs />} />
+            <Route path="view-applications" element={<ViewApplications />} />
+          </Route>
+             :
+          <Route path="/dashboard" element={<RecruiterLogin onClose={handleCloseRecruiterLogin} />} />
+        }
       </Routes>
     </div>
   )
