0535fdac72d46b4f4f9913937b193b9e274b2a3f
elguapo87 <pglogovac@gmail.com>
2025-04-24 14:24:28 +0200

    Added recruiter logout in Navbar component

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -9,34 +9,51 @@ const Navbar = () => {
   const { openSignIn } = useClerk();
   const { user } = useUser();
 
+  const context = useContext(AppContext);
+  if (!context) throw new Error("Navbar must be within AppContextProvider");
+  const { setShowRecruiterLogin, companyToken, setCompanyToken, setCompanyData } = context;
+
   const navigate = useNavigate();
 
-  const context = useContext(AppContext);                                           
-  if (!context) throw new Error("Navbar must be inside AppContextProvider");        
+  const logout = () => {
+    localStorage.removeItem("companyToken");
+    setCompanyToken(null);
+    setCompanyData(null);
+  };
 
-  const { setShowRecruiterLogin } = context;                                         
-  
   return (
     <div className="shadow py-4">
-        <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center">
-          <img onClick={() => navigate("/")} className="cursor-pointer" src={assets.logo} alt="" />
+      <div className="container px-4 2xl:px-20 mx-auto flex items-center justify-between">
+        <img onClick={() => navigate("/")} className="cursor-pointer w-[130px] sm:w-[180px]" src={assets.company_logo} alt="" />
 
+        {
+          user 
+           ?
+          <div className="flex items-center gap-3">
+            <Link to="/applications">Applied Jobs</Link>
+            <p>|</p>
+            <p className="max-sm:hidden">Hello, {" "} {user.firstName && user.lastName ? `${user.firstName} ${user.lastName}` : user.firstName || "John Doe"}</p>
+            <UserButton />
+          </div>
+           :
+        <div className="flex items-center gap-4 max-sm:text-xs">
           {
-            user
-             ?
-            <div className="flex items-center gap-3">
-              <Link to="/applications">Applied Jobs</Link>
-              <p>|</p>
-              <p className="max-sm:hidden">Hy, {user.firstName && user.lastName ? user.firstName + " " + user.lastName : "John Doe"}</p>
-              <UserButton />
-            </div>
-             :
-            <div className="flex gap-4 max-sm:text-xs">
-              <button onClick={() => setShowRecruiterLogin(true)} className="text-gray-600">Recruiter Login</button>                    
-              <button onClick={() => openSignIn()} className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full ">Login</button>
-            </div>
+            companyToken
+                ?
+            <>
+              <button onClick={() => navigate("/dashboard")} className="bg-blue-500 text-white px-3 py-1.5 rounded">Dashboard</button>
+              <button onClick={logout} className="bg-yellow-500 text-white px-2 py-1 rounded text-sm max-sm:text-xs">Logout</button>
+            </>
+                :
+            <>
+              <button onClick={() => setShowRecruiterLogin(true)} className="text-gray-600">Recruiter Login</button>
+              <button onClick={() => openSignIn()} className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full">Login</button>   
+            </>
           }
         </div>
+        }
+
+      </div>
     </div>
   )
 }
