5bb04d8716bdbd5ee0a9984ce626e7ad610b6082
elguapo87 <pglogovac@gmail.com>
2025-04-24 14:44:04 +0200

    Added dynamic data in recruiter Dashboard component & fixed logout logic

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,62 +1,86 @@
-import { useState } from "react"
-import { assets } from "../assets/assets"
-import { NavLink, Outlet, useNavigate } from "react-router-dom";
+import { NavLink, Outlet, useNavigate } from 'react-router-dom'
+import { assets } from '../assets/assets'
+import { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const Dashboard = () => {
 
-  const [showLogout, setShowLogout] = useState(false);
+  const context = useContext(AppContext);
+  if (!context) throw new Error("Dashboard must be within AppContextProvider");
+  const { companyData, setCompanyToken, setCompanyData } = context;
+
+  const [showLogut, setShowLogout] = useState(false);
+
   const navigate = useNavigate();
 
+  const logout = () => {
+    localStorage.removeItem("companyToken");
+    setCompanyToken(null);
+    setCompanyData(null);
+    navigate("/");
+  };
+
+  useEffect(() => {
+    if (companyData) {
+      navigate("/dashboard/manage-jobs");
+    }
+  }, [companyData]);
+
   return (
-    <div className="min-h-screen">
+    <div className='min-h-screen'>
       {/* NAVBAR FOR RECRUITER PANEL */}
-      <div className="shadow py-4">
-        <div className="px-5 flex items-center justify-between">
-          <img onClick={() => navigate("/")} className="max-sm:w-32 cursor-pointer" src={assets.logo} alt="" />
-
-          <div className="flex items-center gap-3">
-            <p className="max-sm:hidden">Welcome, Job Recruiter</p>
-
-            <div className="relative cursor-pointer" onClick={() => setShowLogout(prev => !prev)}>
-              <img className="w-8 border rounded-full" src={assets.company_icon} alt="" />
-
-              {
-                showLogout
-                &&
-                <div className="absolute top-0 right-0 z-10 text-black rounded pt-12">
-                  <ul className="list-none m-0 p-2 bg-white rounded-md border text-sm">
-                    <li className="py-1 px-2 cursor-pointer pr-10">Logout</li>
-                  </ul>
-                </div>
-              }
-            </div>
+      <div className='py-2 sm:py-4 shadow'>
+        <div className='px-5 flex items-center justify-between'>
+          <div className='flex items-end'>
+            <img onClick={() => navigate("/")} src={assets.company_logo} className='w-[130px] sm:w-[180px]  cursor-pointer ' alt="" />
+            <p className='text-xs max-sm:-ml-5 max-sm:scale-[0.55] scale-[0.85] bg-blue-100 px-2.5 py-1 rounded-full font-semibold italic'>Recruiter Panel</p>
           </div>
+
+          {
+            companyData
+                &&
+            <div className='flex items-center gap-3'>
+              <p className='max-sm:hidden'>Welcome, {companyData?.name}</p>
+
+              <div className="relative cursor-pointer" onClick={() => setShowLogout(prev => !prev)}>
+                <img className='w-8 border rounded-full aspect-square' src={companyData.image || assets.company_icon} alt="" />
+
+                {
+                  showLogut
+                    &&
+                  <div className='absolute top-0 right-0 z-10 text-black rounded pt-12'>
+                    <button onClick={logout} className='px-3 py-1.5 bg-yellow-500 text-white rounded border-none text-sm'>Logout</button>
+                  </div>
+                }
+              </div>
+            </div>
+          }
         </div>
       </div>
 
       <div className="flex items-start">
         {/* LEFT SIDEBAR */}
-        <div className="inline-block min-h-screen border-r-2">
-          <ul className="flex flex-col items-start pt-5 text-gray-800">
-            <NavLink className={({isActive}) => `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && "bg-blue-100 border-r-4 border-blue-500"}`} to="/dashboard/add-job">
-              <img className="min-w-4" src={assets.add_icon} alt="" />
-              <p className="max-sm:hidden">Add Job</p>
+        <div className='inline-block min-h-screen border-r-2'>
+          <ul className='flex flex-col gap-3 items-start pt-5 text-gray-800'>
+      
+            <NavLink className={({isActive}) => `flex items-center gap-2 p-3 sm:px-6 w-full hover:bg-gray-100 ${isActive && "bg-blue-100 border-r-4 border-blue-500"}`} to="add-job">
+              <img className='min-w-4 max-sm:max-w-[20px]' src={assets.add_icon} alt="" />
+              <p className='max-sm:hidden lg:text-xl'>Add Job</p>
+            </NavLink>
+       
+            <NavLink className={({isActive}) => `flex items-center gap-2 p-3 sm:px-6 w-full hover:bg-gray-100 ${isActive && "bg-blue-100 border-r-4 border-blue-500"}`} to="manage-jobs">
+              <img className='min-w-4 max-sm:max-w-[20px]' src={assets.home_icon} alt="" />
+              <p className='max-sm:hidden lg:text-xl'>Manage Jobs</p>
             </NavLink>
 
-            <NavLink className={({isActive}) => `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && "bg-blue-100 border-r-4 border-blue-500"}`} to="/dashboard/menage-jobs">
-              <img className="min-w-4" src={assets.home_icon} alt="" />
-              <p className="max-sm:hidden">Menage Jobs</p>
-            </NavLink>
-
-            <NavLink className={({isActive}) => `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && "bg-blue-100 border-r-4 border-blue-500"}`} to="/dashboard/view-applicaitons">
-              <img className="min-w-4" src={assets.person_tick_icon} alt="" />
-              <p className="max-sm:hidden">View Applicaitons</p>
+            <NavLink className={({isActive}) => `flex items-center gap-2 p-3 sm:px-6 w-full hover:bg-gray-100 ${isActive && "bg-blue-100 border-r-4 border-blue-500"}`} to="view-applications">
+              <img className='min-w-4 max-sm:max-w-[20px]' src={assets.person_tick_icon} alt="" />
+              <p className='max-sm:hidden lg:text-xl'>View Applications</p>
             </NavLink>
           </ul>
         </div>
-
-        {/* RIGHT SIDE */}
-        <div>
+        
+        <div className='flex-1 h-full sm:px-5'>
           <Outlet />
         </div>
       </div>
